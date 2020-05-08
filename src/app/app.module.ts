5ac87acfab30ff88fa1a6fd82980cb0818b40da5
collin9726 <owino.collin@gmail.com>
2020-05-08 15:55:11 +0300

    create landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QoutesHomeComponent } from './qoutes-home/qoutes-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QoutesHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/qoutes-home/qoutes-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qoutes-home',
+  templateUrl: './qoutes-home.component.html',
+  styleUrls: ['./qoutes-home.component.css']
+})
+export class QoutesHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
