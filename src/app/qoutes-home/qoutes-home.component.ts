4598476b7751d01feda3ee08ce30b7fb2b5e4e74
collin9726 <owino.collin@gmail.com>
2020-05-08 16:34:27 +0300

    apply Quote constructor in quote-home component

--- src/app/qoutes-home/qoutes-home.component.ts
+++ src/app/qoutes-home/qoutes-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-qoutes-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoutes-home.component.css']
 })
 export class QoutesHomeComponent implements OnInit {
+  quotes: Quote[]=[
+    new Quote('Collin Owino', 'Michelle Muchai', 'Ai Shaaawrry', new Date())
+  ];
 
   constructor() { }
 
