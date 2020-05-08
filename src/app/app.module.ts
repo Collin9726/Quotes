590d0e1a52ffa56065d82f31de6651097e465069
collin9726 <owino.collin@gmail.com>
2020-05-08 16:07:27 +0300

    generate quote-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QoutesHomeComponent } from './qoutes-home/qoutes-home.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QoutesHomeComponent
+    QoutesHomeComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
